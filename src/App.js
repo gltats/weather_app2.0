5304094649f4f9289ef7d471d5f4ca1d68b1f645
Tatiana Sofia Gomes Lima <gltats@Tatianas-MacBook-Air-2.local>
2025-01-19 22:17:02 +0100

    added href that failed in deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 		<Weather />
 		<footer>
 			<p> This project was coded by Sofia and
-				 is open-sourced on <a href="" target="_blank" >GitHub</a> and hosted on <a href="" target="_blank">Netlify</a>. </p>
+				 is open-sourced on <a href="https://github.com/gltats/weather_app2.0" target="_blank" >GitHub</a> and hosted on <a href="https://weather-gltats.netlify.app" target="_blank">Netlify</a>. </p>
 		</footer>
     </div>
   );
